e817cb63a1cbb2af9b0a38642e22e343d0ec63d1
kmmytn <134942694+kmmytn@users.noreply.github.com>
2024-06-19 23:50:57 +0800

    added gameplay, home, and game over; fixed java dependencies

---
+++ app/src/main/java/com/mobdeve/s11/grp9/david/tan/arcticflight/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mobdeve.s11.grp9.david.tan.arcticflight;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+
+    }
+}
