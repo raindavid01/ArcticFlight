eabfea8e3d0a1d277ebf78afc7d9b9ca9a687ad2
raindavid01 <rain_david@dlsu.edu.ph>
2024-06-22 19:57:29 +0800

    added ShopActivity.java

---
+++ app/src/main/java/com/mobdeve/s11/grp9/david/tan/arcticflight/ShopActivity.java
@@ -0,0 +1,51 @@
+package com.mobdeve.s11.grp9.david.tan.arcticflight;
+
+import android.app.Dialog;
+import android.view.Window;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.mobdeve.s11.grp9.david.tan.arcticflight.databinding.HomeBinding;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ShopActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    MyAdapter myAdapter;
+
+    int []arr={R.drawable.s1,R.drawable.s2,R.drawable.s3,R.drawable.s4,R.drawable.s5,
+            R.drawable.s6,R.drawable.s7,R.drawable.s8,R.drawable.s9,R.drawable.s10,
+            R.drawable.s11,R.drawable.s12,R.drawable.s13,R.drawable.s14,R.drawable.s15};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shop);
+        recyclerView = findViewById(R.id.recyclerView);
+        layoutManager = new GridLayoutManager(getApplicationContext(), 3);
+        recyclerView.setLayoutManager(layoutManager);
+        myAdapter = new MyAdapter(arr);
+
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setHasFixedSize(true);
+
+        Button shopbackBtn = findViewById(R.id.shopbackBtn);
+        shopbackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ShopActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
